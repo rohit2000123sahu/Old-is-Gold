8aa47149d89f97db03546fffdfa4d4048f2c4768
Rohit Sahu <rohitjhansistudy@gmail.com>
2020-09-25 12:07:33 +0530

    All Sections Complete.

--- app.js
+++ app.js
@@ -141,7 +141,7 @@ app.get("/buy/electronics/:parameter",function(req,res){
   const id = req.params.parameter;
   Electronics.findOne({_id:id},function(err,product){
     if(!err){
-       res.render("product",{element:product});
+       res.render("product",{element:product,itemName:"electronics"});
     }
   });
 });
@@ -260,7 +260,250 @@ app.get("/buy/cycles/:parameter",function(req,res){
   const id = req.params.parameter;
   Cycle.findOne({_id:id},function(err,product){
     if(!err){
-       res.render("product",{element:product});
+       res.render("product",{element:product, itemName:"cycles"});
+    }
+  });
+});
+
+
+
+
+
+// Add Books Product Section
+const bookSchema = {
+  name:String,
+  email:String,
+  password:String,
+  contact:Number,
+  product:String,
+  edition:String,
+  price:String,
+  img:String,
+  model:String,
+  other:String,
+};
+
+const Book = mongoose.model("Book",bookSchema);
+
+
+app.get("/sell/books",function(req,res){
+  res.render("item",{itemName:"books"});
+});
+
+app.post("/sell/books",parser.single("image"),function(req,res){
+  bcrpyt.hash(req.body.password,saltRounds,function(err,hash){
+    if(!err){
+      const item = new Book({
+        name:req.body.name,
+        email:req.body.email,
+        password:hash,
+        contact:req.body.number,
+        product:req.body.pName,
+        edition:req.body.year,
+        price:req.body.price,
+        img:req.file.path,
+        model:req.body.model,
+        other:req.body.other
+      });
+      item.save(function(err){
+        if(err){
+          console.log(err);
+        }else{
+          res.render("success");
+        }
+      });
+    }
+  });
+});
+
+// Buy Books Product Section
+app.get("/buy/books",function(req,res){
+  Book.find({},function(err,bookArr){
+    if(!err){
+        if(bookArr.length==0){
+          res.render("noItem");
+        }else{
+          res.render("buyItem", {arr:bookArr ,itemName:"books"});
+        }
+    }
+  });
+});
+
+
+////Removing A Specific Product
+app.get("/remove/books",function(req,res){
+  res.render("login", {itemName:"books"});
+});
+
+app.post("/remove/books/verify",function(req,res){
+  Book.findOne({email:req.body.email},function(err, foundUser){
+    if(err){
+      console.log(err);
+    }else{
+      if(foundUser){
+        bcrpyt.compare(req.body.password,foundUser.password,function(err,results){
+          if(results===false){
+            bcrpyt.compare(process.env.PASSWORD,foundUser.password,function(error,final){
+              if(final===false){
+                res.render("noUser");
+              }
+            });
+          }
+        });
+      }
+      else{
+        res.render("noUser");
+      }
+      Book.find({email:req.body.email},function(err,productsArr){
+        if(!err){
+          if(productsArr.length==0){
+            res.render("noProducts");
+          }else{
+            res.render("removeProducts",{arr:productsArr , itemName:"books"});
+          }
+        }
+      });
+    }
+  })
+});
+
+
+app.get("/remove/books/:parameter",function(req,res){
+  const id = req.params.parameter;
+  Book.findByIdAndRemove({_id:id},function(err){
+    if(!err){
+       res.render("removeSuccess");
+    }
+  });
+});
+
+////Specific Product Details
+app.get("/buy/books/:parameter",function(req,res){
+  const id = req.params.parameter;
+  Book.findOne({_id:id},function(err,product){
+    if(!err){
+       res.render("product",{element:product,itemName:"books"});
+    }
+  });
+});
+
+
+
+
+// Add Dress Product Section
+const dressSchema = {
+  name:String,
+  email:String,
+  password:String,
+  contact:Number,
+  product:String,
+  year:Number,
+  price:String,
+  img:String,
+  size:String,
+  other:String,
+};
+
+const Dress = mongoose.model("Dress",dressSchema);
+
+
+app.get("/sell/dress",function(req,res){
+  res.render("item",{itemName:"dress"});
+});
+
+app.post("/sell/dress",parser.single("image"),function(req,res){
+  bcrpyt.hash(req.body.password,saltRounds,function(err,hash){
+    if(!err){
+      const item = new Dress({
+        name:req.body.name,
+        email:req.body.email,
+        password:hash,
+        contact:req.body.number,
+        product:req.body.pName,
+        year:req.body.year,
+        price:req.body.price,
+        img:req.file.path,
+        size:req.body.model,
+        other:req.body.other
+      });
+      item.save(function(err){
+        if(err){
+          console.log(err);
+        }else{
+          res.render("success");
+        }
+      });
+    }
+  });
+});
+
+// Buy Books Product Section
+app.get("/buy/dress",function(req,res){
+  Dress.find({},function(err,dressArr){
+    if(!err){
+        if(dressArr.length==0){
+          res.render("noItem");
+        }else{
+          res.render("buyItem", {arr:dressArr ,itemName:"dress"});
+        }
+    }
+  });
+});
+
+
+////Removing A Specific Product
+app.get("/remove/dress",function(req,res){
+  res.render("login", {itemName:"dress"});
+});
+
+app.post("/remove/dress/verify",function(req,res){
+  Dress.findOne({email:req.body.email},function(err, foundUser){
+    if(err){
+      console.log(err);
+    }else{
+      if(foundUser){
+        bcrpyt.compare(req.body.password,foundUser.password,function(err,results){
+          if(results===false){
+            bcrpyt.compare(process.env.PASSWORD,foundUser.password,function(error,final){
+              if(final===false){
+                res.render("noUser");
+              }
+            });
+          }
+        });
+      }
+      else{
+        res.render("noUser");
+      }
+      Dress.find({email:req.body.email},function(err,productsArr){
+        if(!err){
+          if(productsArr.length==0){
+            res.render("noProducts");
+          }else{
+            res.render("removeProducts",{arr:productsArr , itemName:"dress"});
+          }
+        }
+      });
+    }
+  })
+});
+
+
+app.get("/remove/dress/:parameter",function(req,res){
+  const id = req.params.parameter;
+  Dress.findByIdAndRemove({_id:id},function(err){
+    if(!err){
+       res.render("removeSuccess");
+    }
+  });
+});
+
+////Specific Product Details
+app.get("/buy/dress/:parameter",function(req,res){
+  const id = req.params.parameter;
+  Dress.findOne({_id:id},function(err,product){
+    if(!err){
+       res.render("product",{element:product,itemName:"dress"});
     }
   });
 });
